4062ab4c1dde6eb013ea560f91a2ef6c96c394ac
happa114514 <98687541+happa114514@users.noreply.github.com>
2023-01-28 22:59:55 +0800

    Add files via upload

--- public/setting/setting.js
+++ public/setting/setting.js
@@ -1,3 +1,4 @@
+
 //主题信息设置
-var themeName = "在/public/setting/setting.js中设置主题名称";   //主题名称
-var themeAuthor = "在/public/setting/setting.js中设置主题作者";  //主题作者
+var themeName = "鲸歌";
+var themeAuthor = "つつ";
